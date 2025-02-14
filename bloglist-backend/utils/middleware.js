b2488930cfc592d7b759e1771e11591ee99f6473
Anon2148 <privext@tutanota.com>
2025-02-14 18:11:59 +0100

    fix: blogs were not shown during first sign-in, token extractor middleware fixed

--- bloglist-backend/utils/middleware.js
+++ bloglist-backend/utils/middleware.js
@@ -15,18 +15,28 @@ const tokenExtractor = (request, response, next) => {
 
   if (authorization && authorization.startsWith('Bearer ')) {
     request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
   }
   next()
 }
 
 const userExtractor = async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  } else {
-    request.user = await User.findById(decodedToken.id)
+  if (!request.token) {
+    request.user = null
+    return next()
   }
-  next()
+
+  try {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    request.user = await User.findById(decodedToken.id)
+    next()
+  } catch (error) {
+    next(error)
+  }
 }
 
 const unknownEndpoint = (request, response) => {
