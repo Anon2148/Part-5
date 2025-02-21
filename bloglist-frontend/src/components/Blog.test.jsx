fad5d4456c01ce4dd2acdc9425036383b1428c4a
Anon2148 <privext@tutanota.com>
2025-02-21 13:59:41 +0100

    feat: exercise 5.16, Blog List Tests step 4 done

--- bloglist-frontend/src/components/Blog.test.jsx
+++ bloglist-frontend/src/components/Blog.test.jsx
@@ -2,10 +2,13 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import { expect, vi } from 'vitest'
+import NewBlogForm from './NewBlogForm'
+import Togglable from './Togglable'
 
 describe('<Blog />', () => {
   let container
   let mockHandler
+  let newBlogMockHandler
   const blog = {
     title: 'new blog',
     author: 'author',
@@ -16,7 +19,15 @@ describe('<Blog />', () => {
 
   beforeEach(() => {
     mockHandler = vi.fn()
-    container = render(<Blog blog={blog} addLike={mockHandler} />).container
+    newBlogMockHandler = vi.fn()
+    container = render(
+      <>
+        <Togglable buttonLabel="new blog">
+          <NewBlogForm createBlog={newBlogMockHandler} />
+        </Togglable>
+        <Blog blog={blog} addLike={mockHandler} />
+      </>
+    ).container
   })
 
   test('at start only blog`s title and blog`s author are displayed', async () => {
@@ -46,6 +57,30 @@ describe('<Blog />', () => {
     await user.dblClick(likeButton)
 
     expect(mockHandler.mock.calls).toHaveLength(2)
+  })
+
+  test('create a blog should call the function passed as a parameter to the object', async () => {
+    const user = userEvent.setup()
+    const newBlogButton = screen.getByText('new blog')
+    const createBlogButton = screen.getByText('create')
+    const inputTitle = container.querySelector("input[name='title']")
+    const inputAuthor = container.querySelector("input[name='author']")
+    const inputUrl = container.querySelector("input[name='url']")
+
+    await user.click(newBlogButton)
+    await user.type(inputTitle, 'test title')
+    await user.type(inputAuthor, 'test author')
+    await user.type(inputUrl, 'test url')
+    await user.click(createBlogButton)
+
+    console.log(newBlogMockHandler.mock.calls)
+
+    expect(newBlogMockHandler.mock.calls).toHaveLength(1)
+    expect(newBlogMockHandler.mock.calls[0][0]).toMatchObject({
+      title: 'test title',
+      author: 'test author',
+      url: 'test url',
+    })
   })
 })
 
